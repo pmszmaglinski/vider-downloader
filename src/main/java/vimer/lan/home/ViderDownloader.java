3ef6b0ea962d0aca2c42d3394a16295e6c988656
piotr.szmaglinski <piotr.szmaglinski@jeppesen.com>
2021-12-23 14:42:48 +0100

    Befor refactoring to one loop

--- src/main/java/vimer/lan/home/ViderDownloader.java
+++ src/main/java/vimer/lan/home/ViderDownloader.java
@@ -7,57 +7,62 @@ import jodd.http.HttpResponse;
 import jodd.io.FileUtil;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 class ViderDownloader {
 
     //final String searchUrl = "https://vider.info/search/all/{searchTerms}";
     final String viderUrl = "https://vider.info";
-    final String sesonsPath = "/dir/+dnv1mm";
+    final String seriesPath = "/dir/+dnv1mm";
     final static String tesseractDatapath = "/usr/local/Cellar/tesseract/4.1.3/share/tessdata";
     final static String tesseractLanguage = "eng";
     final static String tesseractImageDPI = "96";
-    Map<String, String> sesonsMap = new LinkedHashMap<>();
-    Map<String, String> moviesPathMap = new LinkedHashMap<>();
+    Map<String, String> sesonsPathMap = new LinkedHashMap<>();
+    Map<String, String> episodesIntermediatePathMap = new LinkedHashMap<>();
+    JSONObject configuration = new JSONObject();
 
-
+    
     public static void main(String[] args) throws TesseractException, IOException {
         ViderDownloader viderDownloader = new ViderDownloader();
-        viderDownloader.getSesonsDirURLs()
-                .getMoviesPath()
-                .getMoviesEncodedUrl();
+        viderDownloader.generateSesonsPathsMap()
+                .generateEpisodesIntermediatePathsMap()
+                .generateEpisodeDownloadLinksMap();
     }
 
-    ViderDownloader getSesonsDirURLs() throws IOException, TesseractException {
+    ViderDownloader generateSesonsPathsMap() throws IOException, TesseractException {
         Document doc;
-        String sesonsUrl = viderUrl + sesonsPath;
-        HttpResponse response = HttpRequest.get(sesonsUrl).send();
+        String seriesUrl = viderUrl + seriesPath;
+        HttpResponse response = HttpRequest.get(seriesUrl).send();
 
         if (response.statusCode() == 404) {
             doc = Jsoup.parse(response.toString());
-            response = fixCaptcha(sesonsUrl, doc, response);
+            response = fixCaptcha(seriesUrl, doc, response);
         }
 
         doc = Jsoup.parse(response.toString());
-        getElementsMap(doc, this.sesonsMap);
+        addSeriesNameToConfiguration(doc, configuration);
+        addElementsToMap(doc, this.sesonsPathMap);
 
         return this;
     }
 
-    ViderDownloader getMoviesPath() {
-        sesonsMap.forEach( (sesonName, sesonPath) -> {
+    ViderDownloader generateEpisodesIntermediatePathsMap() {
+        sesonsPathMap.forEach( (sesonName, sesonPath) -> {
             Document doc;
             String sesonUrl = viderUrl + sesonPath;
 
             if (sesonName.equals("Sezon 1")) {
-                System.out.println(sesonUrl);
+                System.out.println(sesonName + " --> " + sesonUrl);
                 HttpResponse response = HttpRequest.get(sesonUrl).send();
 
                 if (response.statusCode() == 404) {
@@ -70,48 +75,51 @@ class ViderDownloader {
                 }
 
                 doc = Jsoup.parse(response.toString());
-                getElementsMap(doc, this.moviesPathMap);
+                addElementsToMap(doc, this.episodesIntermediatePathMap);
             }
         });
 
         return this;
     }
 
-    ViderDownloader getMoviesEncodedUrl() {
-        moviesPathMap.forEach((sesonName, sesonPath) -> {
+    ViderDownloader generateEpisodeDownloadLinksMap() {
+        episodesIntermediatePathMap.forEach((episodeName, episodePath) -> {
             Document doc;
-            String movieUrl = viderUrl + sesonPath;
+            String episodeIntermediate1Url = viderUrl + episodePath;
+            System.out.println("episodeIntermediate1Url : " + episodeIntermediate1Url);
 
-            //if (sesonPath.equals("/vid/+fnns5xx")) {    // Konkretny odcinek - debugowanie
-                HttpResponse response = HttpRequest.get(movieUrl).send();
+            //if (episodePath.equals("/vid/+fnns5xx")) {    // Konkretny odcinek - debugowanie
+                HttpResponse response = HttpRequest.get(episodeIntermediate1Url).send();
                 if (response.statusCode() == 404) {
                     doc = Jsoup.parse(response.toString());
                     try {
-                        response = fixCaptcha(movieUrl, doc, response);
+                        response = fixCaptcha(episodeIntermediate1Url, doc, response);
                     } catch (IOException | TesseractException e) {
                         e.printStackTrace();
                     }
                 }
 
                 doc = Jsoup.parse(response.toString());
-                String videoSrcUrl = getVideoSrcUrl(doc);
-                System.out.println(videoSrcUrl);
+                String episodeIntermediate2Url = getepisodeIntermediate2Url(doc);
+                System.out.println("episodeIntermediate2Url : " + episodeIntermediate2Url);
 
-                response = HttpRequest.get(videoSrcUrl)
+                response = HttpRequest.get(episodeIntermediate2Url)
                                     .header("referer","https://vider.info/")
                                     .send();
 
                 if (response.statusCode() == 404) {
                     doc = Jsoup.parse(response.toString());
                     try {
-                        response = fixCaptcha(movieUrl, doc, response);
+                        response = fixCaptcha(episodeIntermediate1Url, doc, response);
                     } catch (IOException | TesseractException e) {
                         e.printStackTrace();
                     }
                 }
 
-                String redirectedMovieLink = response.header("Location");
-                System.out.println(redirectedMovieLink);
+                String episodeDownloadLink = response.header("Location");
+                System.out.println("episodeDownloadLink : " + episodeDownloadLink);
+
+                //TODO: - Generate json with {Seson 1:{{episode1Name,episodeUrl},{episode2Name,episodeUrl}},Seson2:{...}
 
                 //TODO: - Download file (async?) - kilka jednocześnie ?
                 //      - Refactor captcha calling
@@ -125,20 +133,26 @@ class ViderDownloader {
 
             //}
         });
+        System.out.println("sesonsPathMap: " + sesonsPathMap);
+        System.out.println("episodesIntermediatePathMap: " + episodesIntermediatePathMap);
 
         return this;
     }
 
-    private String getVideoSrcUrl(Document doc) {
+    private String getepisodeIntermediate2Url(Document doc) {
         return doc.select("link[rel=video_src]")
                 .attr("href")
                 .replaceAll("^.*file=","");
     }
 
-    private void getElementsMap(Document doc, Map<String,String> map) {
-        Elements sesonsURI = doc.select("p.title > a");
-        sesonsURI.forEach( element -> map.put(element.html(), element.attr("href")));
+    private void addSeriesNameToConfiguration(Document doc, JSONObject configuration) {
+        String el = Objects.requireNonNull(doc.select("title").first()).text();
+        configuration.put("title", el);
+    }
 
+    private void addElementsToMap(Document doc, Map<String,String> map) {
+        Elements el = doc.select("p.title > a");
+        el.forEach( element -> map.put(element.html(), element.attr("href")));
     }
 
     private static Tesseract getTesseract() {
